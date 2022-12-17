ec38b5842d52ab4c2650771175c6d98cca63a2e2
David Zambo <dave@dcmf.io>
2022-12-17 22:00:17 +0100

    working on the next step checking

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 unsigned short ** init_table();
 
+bool check_has_next_step(unsigned short **pInt);
+
 unsigned short **init_table() {
   unsigned short **table = malloc(5 * sizeof(short *));
 
@@ -23,6 +25,41 @@ unsigned short **init_table() {
 
 int main() {
   unsigned short **table = init_table();
-
+  bool has_next_step = check_has_next_step(table);
   printf("ez");
+}
+
+bool check_has_next_step(unsigned short **table) {
+ int i, j, sum = 0;
+ bool has_pug_to_jump_with = false;
+
+ for (i = 0; i < 5; i++) {
+   for (j = 0; j < 5; j++) {
+     sum += table[i][j];
+
+     if (!has_pug_to_jump_with) {
+       if (
+               (table[i][j] == 1 && i >= 2 && table[i-1][j] == 1 && table[i-2][j] == 0) ||
+               (table[i][j] == 1 && i >= 2 && j >= 2 && table[i-1][j-1] == 1 && table[i-2][j-2] == 0) ||
+               (table[i][j] == 1 && j >= 2 && table[i][j-1] == 1 && table[i][j-2] == 0) ||
+               (table[i][j] == 1 && i <= 2 && j >= 2 && table[i-1][j+1] == 1 && table[i-2][j+2] == 0) ||
+               (table[i][j] == 1 && i <= 2 && table[i+1][j] == 1 && table[i+2][j] == 0) ||
+               (table[i][j] == 1 && i <= 2 && j <= 2 && table[i+1][j+1] == 1 && table[i+2][j+2] == 0) ||
+               (table[i][j] == 1 && j <= 2 && table[i][j+1] == 1 && table[i][j+2] == 0) ||
+               (table[i][j] == 1 && i <= 2 && j >= 2 && table[i+1][j-1] == 1 && table[i+2][j-2] == 0)
+             )
+       {
+         has_pug_to_jump_with = true;
+         break;
+       }
+     }
+   }
+ }
+ sum -=  (table[0][0] + table[0][4] + table[4][0] + table[4][4]);
+
+ if (sum == 1) {
+   return false;
+ }
+
+  return has_pug_to_jump_with;
 }
